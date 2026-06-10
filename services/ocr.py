2c96fe207b16f2057866ef2ffd8314c72753001c
Rohit Jha <Rohitjha91748@gmail.com>
2026-06-10 16:00:34 +0530

    Disable OCR

--- services/ocr.py
+++ services/ocr.py
@@ -1,13 +1,2 @@
 def extract_text(image_path):
     return ["OCR disabled for deployment test"]
-
-def extract_text(image_path):
-
-    result = reader.readtext(image_path)
-
-    text = []
-
-    for item in result:
-        text.append(item[1])
-
-    return text
